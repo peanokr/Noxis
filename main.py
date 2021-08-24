d7679af3aa4be79115d74709f30860afae2a09fe
jobert <wasdodo@gmail.com>
2021-08-23 20:51:35 -0700

    nothing

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from tkinter import *
+import beepy
 
 
 if __name__ == '__main__':
 
+def play_sound():
+
+
     root = Tk()
     root.title("WHACK THAT GACK")
     root.geometry('350x200')
+
+    lbl = Label(root, text = "Arses")
+    lbl.grid()
+
+    btn = Button(root, text = "Click Me", fg = "red", command = play_sound())
+
     root.mainloop()
 
